39da0b72735b66d487b96fcc5a7f4f4d85ec6efa
Panashe Mugayapi <pmugayapi@hotmail.com>
2022-09-29 22:27:46 +1000

    Creating a Vue profile to learn basics of Vue3

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+  <header>
+    <h1>Panashe Mugayapi</h1>
+    <h2>Software Engineer</h2>
+  </header>
+  <main>
+    <p>Full Stack Development using</p>
+    <ul>
+      <li>HTML5</li>
+      <li>CSS3</li>
+      <li>JS6</li>
+      <li>PHP</li>
+      <li>Laravel</li>
+      <li>Vue3</li>
+    </ul>
+  </main>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {}
+</style>
